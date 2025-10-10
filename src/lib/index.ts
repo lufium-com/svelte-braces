cc895d5b0c8ae9d8c9c434af2da47dad58d3ab4d
Andrej Vckovski <andrej@lufium.com>
2025-10-10 10:54:04 +0200

    - changed exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,6 @@
-// Reexport your entry components here
+// Reexport the component and other useful items
+// the deafult export is so that you can write import ... from 'svelte-runtime-template'
+export { default } from '$lib/TemplateRenderer.svelte'
 export { default as TemplateRenderer } from '$lib/TemplateRenderer.svelte'
 export {defaultErrorReplacer, defaultTextReplacer } from '$lib/TemplateRenderer.svelte'
 export * from '$lib/parser.js'
